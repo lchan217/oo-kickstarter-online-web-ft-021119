136e363bbd78be0aed2af38a7ca865d089944953
Lizzie Chan <lchan217@gmail.com>
2019-03-06 22:51:35 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -6,8 +6,4 @@ class Backer
     @name = name 
     @backed_projects = []
   end
-  def back_project(project_instance)
-    @backed_projects << project_instance 
-    project_instance.backers << self  
-  end 
 end 
